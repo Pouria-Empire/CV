d2c34c27f88492d0132661a31535bdebf8e830d8
pouria <arefijamal.pouria@gmail.com>
2022-07-28 19:14:53 +0430

    first commit

---
+++ 2.cpp
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+#include <map>
+
+struct Edge
+{
+	int v ;
+	int flow ;
+	int C; 
+	int rev ; 
+};
+
+class Graph
+{
+	int V;
+	int *level ; 
+	vector< Edge > *adj;
+public :
+	Graph(int V)
+	{
+		adj = new vector<Edge>[V];
+		this->V = V;
+		level = new int[V];
+	}
+
+	void addEdge(int u, int v, int C)
+	{
+		Edge a{v, 0, C, int(adj[v].size())};
+
+		Edge b{u, 0, 0, int(adj[u].size())};
+
+		adj[u].push_back(a);
+		adj[v].push_back(b); 
+	}
+
+	bool BFS(int s, int t);
+	int sendFlow(int s, int flow, int t, int ptr[]);
+	int DinicMaxflow(int s, int t);
+};
+
+bool Graph::BFS(int s, int t)
+{
+	for (int i = 0 ; i < V ; i++)
+		level[i] = -1;
+
+	level[s] = 0; 
+
+	list< int > q;
+	q.push_back(s);
+
+	vector<Edge>::iterator i ;
+	while (!q.empty())
+	{
+		int u = q.front();
+		q.pop_front();
+		for (i = adj[u].begin(); i != adj[u].end(); i++)
+		{
+			Edge &e = *i;
+			if (level[e.v] < 0 && e.flow < e.C)
+			{
+				level[e.v] = level[u] + 1;
+
+				q.push_back(e.v);
+			}
+		}
+	}
+
+	return level[t] < 0 ? false : true ;
+}
+
+
+int Graph::sendFlow(int u, int flow, int t, int start[])
+{
+	if (u == t)
+		return flow;
+	for ( ; start[u] < adj[u].size(); start[u]++)
+	{
+		Edge &e = adj[u][start[u]];
+									
+		if (level[e.v] == level[u]+1 && e.flow < e.C)
+		{
+			int curr_flow = min(flow, e.C - e.flow);
+
+			int temp_flow = sendFlow(e.v, curr_flow, t, start);
+
+			if (temp_flow > 0)
+			{
+				e.flow += temp_flow;
+
+				adj[e.v][e.rev].flow -= temp_flow;
+				return temp_flow;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int Graph::DinicMaxflow(int s, int t)
+{
+	if (s == t)
+		return -1;
+
+	int total = 0; 
+
+	while (BFS(s, t) == true)
+	{
+		int *start = new int[V+1] {0};
+		while (int flow = sendFlow(s, INT_MAX, t, start))
+			total += flow;
+	}
+	return total;
+}
+
+int main()
+{
+	int n,m;
+    scanf(" %d %d",&n,&m);
+
+    int node_weights[1010] = {0};
+     
+    for(int i=0;i<n;i++)
+        scanf(" %d",&node_weights[i]);
+    
+    map<int, vector<int>> adjacency;
+    for(int i=1;i<n+1;i++){
+        vector<int> t;
+        adjacency[i] = t;
+    }
+
+
+    vector<int> weightsOfEdges;
+    int sum = 0;
+    for(int i=1;i<m+1;i++){
+        int u,v,w;
+        scanf(" %d %d %d",&u,&v,&w);
+        adjacency[u].push_back(i);
+        adjacency[v].push_back(i);
+        weightsOfEdges.push_back(w);
+        sum+=w;
+    }
+
+    auto g = Graph(m+n+2);
+
+    for(int i=1;i<n+1;i++){
+        g.addEdge(0,i,node_weights[i-1]);
+        for(int adj : adjacency[i])
+            g.addEdge(i,n+adj,1000000);
+    }
+
+    for(int i=1;i<m+1;i++)
+        g.addEdge(n + i, n + m + 1, weightsOfEdges[i - 1]);
+    
+    int ans = g.DinicMaxflow(0, n + m + 1);
+
+    printf("%d",sum-ans);
+
+	return 0;
+}
